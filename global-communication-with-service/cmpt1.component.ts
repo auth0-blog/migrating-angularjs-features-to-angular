5aaf1e29e082bcf3a9dd54ef1fa4a409ba76fa08
Kim Maida <kim.maida@auth0.com>
2016-11-21 09:29:28 -0500

    Add subscription to counter example.

--- global-communication-with-service/cmpt1.component.ts
+++ global-communication-with-service/cmpt1.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 import { CounterService } from './counter.service';
 
 @Component({
@@ -12,10 +13,20 @@ import { CounterService } from './counter.service';
     <div>
   `
 })
-export class Cmpt1Component implements OnInit {
+export class Cmpt1Component implements OnInit, OnDestroy {
+  countSub: Subscription;
   
   constructor(private counter: CounterService) { }
 
   ngOnInit() {
+    this.countSub = this.counter.count$.subscribe(
+      value => {
+        console.log('counter value changed:', value);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.countSub.unsubscribe();
   }
 }
