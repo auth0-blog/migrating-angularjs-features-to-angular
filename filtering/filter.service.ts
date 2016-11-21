9b5fa6ff09b5efa9b5b46cc8841c25d919561dea
Kim Maida <kim.maida@auth0.com>
2016-11-21 11:28:24 -0500

    Add filtering samples.

---
+++ filtering/filter.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class FilterService {
+  search(array: Object[], query: string) {
+    let lQuery = query.toLowerCase();
+
+    if (!query) {
+      return array;
+    } else if (array) {
+      let filteredArray = array.filter(item => {
+        for (let key in item) {
+          if ((typeof item[key] === 'string') && (item[key].toLowerCase().indexOf(lQuery) !== -1)) {
+            return true;
+          }
+        }
+      });
+      return filteredArray;
+    }
+  }
+
+}
---
+++ filtering/list.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FilterService } from './filter.service';
+
+@Component({
+  selector: 'app-list',
+  template: `
+    <label for="search">Search:</label>
+    <input id="search" type="text" [(ngModel)]="query" />
+    <button (click)="search()" [disabled]="!query">Go</button>
+    <button (click)="reset()" [disabled]="!query">Reset</button>
+
+    <ul>
+      <li *ngFor="let item of filteredArray">{{item.name}}</li>
+    </ul>
+  `
+})
+export class ListComponent {
+  array: Object[] = [
+    { id: 1, name: 'Jon Snow' },
+    { id: 2, name: 'Sansa Stark' },
+    { id: 3, name: 'Arya Stark' },
+    { id: 4, name: 'Bran Stark' },
+    { id: 5, name: 'Petyr Baelish' },
+    { id: 6, name: 'Danaerys Targaryen' },
+    { id: 7, name: 'Jaime Lannister ' },
+    { id: 8, name: 'Cersei Lannister' },
+    { id: 9, name: 'Samwell Tarly' },
+    { id: 10, name: 'Sandor Clegane' }
+  ];
+  filteredArray: Object[] = this.array;
+  query: string;
+  
+  constructor(private filter: FilterService) { }
+
+  search() {
+    this.filteredArray = this.filter.search(this.array, this.query);
+  }
+  reset() {
+    this.query = '';
+    this.filteredArray = this.array;
+  }
+}
